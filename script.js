514a3b242cb7ba524e3856b5abd19c4a8539c49e
silvio <150728497+silspp@users.noreply.github.com>
2024-08-06 19:24:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-Alternativas");
+const caixaResultados=document.querySelector(".caixa-resultados");
+const textoResultado=document.querySelector(".texto-resultado");
+const perguntas = [ // abre lista de perguntas
+{ // abre objeto de perguntas
+ enunciado:"",
+    alternativas:[
+        "",
+        "",
+    ]// fecha lista de perguntas
+}, // fecha objeto de perguntas
+
+{
+    enunciado :"",
+    alternativas :[
+      "",
+
+     " ",
+
+    ] 
+},
+     {
+        enunciado:"",
+        alternativas:[
+            "Verdade",
+            "Mentira",
+        ]
+     }
+];
+
